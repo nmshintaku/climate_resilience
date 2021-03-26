54a0127111475f546ac8237f737ec952005431b4
davidgillmarine <gilldavida@gmail.com>
2021-03-26 18:43:41 -0400

    - script to calculate anomalies using stars package in R

---
+++ stars_ind_climate.R
@@ -0,0 +1,101 @@
+#install.packages('pacman') # run once
+
+# load packages and set directories
+pacman::p_load(rnaturalearth,stars,cubelyr, viridis,lubridate,ggthemes,tidyverse)
+pcdir <- "R:/Gill/research/climate-resilience"
+inputdir <- paste0(pcdir, "/Indonesia_NS/Data/")
+outputdir <- paste0(pcdir, "/Indonesia_NS/Data/")
+
+# --- read in data ----
+#list.files(inputdir)
+r = read_stars(paste0(inputdir,"1979-2020_allvariables.nc"))
+indonesia <- ne_countries(scale = 110, country = 'indonesia',returnclass = "sf") %>% 
+  st_crop(st_bbox(r))
+plot(st_geometry(indonesia))
+
+
+# --- Yearly values ----
+# get time values
+time.val <- st_get_dimension_values(r, "time")
+yr.time.val <- unique(year(st_get_dimension_values(r, "time"))) # get time values in years
+yr.time.val
+
+# Aggregate by year
+by_t = "1 year"   # set time intervals
+r.agg.yr <- aggregate(r, by = by_t, FUN = mean) # average values by year
+r.agg.yr <- st_set_dimensions(r.agg.yr, "time", values = yr.time.val, names = "year") # convert date to years
+
+# Anomaly function
+fn_anom <-  function(x) ((x-mean(x, na.rm=T))/sd(x,na.rm=T)) # function to calculate anomalies (in standard deviation units as example)
+
+# Aggregate over all years
+r.agg.all.yrs <- st_apply(r.agg.yr, 2:3, fn_anom) # calculate the anomaly over all years (1979-2020)
+r.agg.all.yrs <- st_set_dimensions(r.agg.all.yrs, "fn_anom", values = yr.time.val, names = "year") # rename values and band
+
+# Subset to 2009-2019 data
+wanted.yrs <- which(yr.time.val%in%c(2009:2019))
+r.sub <- r.agg.all.yrs[,wanted.yrs,,] 
+
+#Sample plot
+ggplot() + 
+  geom_stars(data=select(r.sub,si10)) +
+  coord_equal() +
+  facet_wrap(.~year) +
+  scale_fill_viridis(option="D") +
+  theme_void() +
+  geom_sf(data = indonesia, fill = "gray95") +
+  labs(title="Si10")
+
+# --- Monthly anomalies ----
+# get time values
+time.val <- st_get_dimension_values(r, "time") # get time stamps
+month.val <- seq(1,length(time.val),12) # pull out the Januarys
+month.val
+yr.time.val <- unique(year(st_get_dimension_values(r, "time"))) # get time values in years
+
+r.jan <- r[,,,month.val]
+st_get_dimension_values(r.jan, "time") # see if it worked
+
+# Anomaly function
+fn_anom <-  function(x) ((x-mean(x, na.rm=T))/sd(x,na.rm=T)) # function to use
+r.jan <- st_apply(r[,,,month.val], 1:2, fn_anom) # calculate the anomaly over all years
+r.jan <- st_set_dimensions(r.jan, "fn_anom", values = paste0("1-", yr.time.val), names = "month") # rename values and band
+
+# Subset to 2009-2019 data
+wanted.yrs <- which(yr.time.val%in%c(2009:2019))
+r.jan.sub <- r.jan[,wanted.yrs,,] 
+
+#Sample plot
+ggplot() + 
+  geom_stars(data=select(r.jan.sub,si10)) +
+  coord_equal() +
+  facet_wrap(.~month) +
+  scale_fill_viridis_c(option = "D")+
+  theme_void() +
+  geom_sf(data = indonesia, fill = "gray95") +
+  labs(title="Si10 - January")
+
+
+# write a function to do the other months
+comb.sub <- r.jan.sub
+for (i in 1:11){
+  print(i)
+  dat <- st_apply(r[,,,month.val + i], 1:2, fn_anom) # calculate the anomaly over all years
+  dat <- st_set_dimensions(dat, "fn_anom", values =  paste0(i+ 1,"-", yr.time.val), names = "month-year") # convert date to years
+  dat <- dat[,wanted.yrs,,] 
+  comb.sub <- c(comb.sub,dat, along = 1)
+} 
+
+st_get_dimension_values(comb.sub, "month")
+
+#Sample plot
+ggplot() + 
+  geom_stars(data=select(comb.sub,sst)) +
+  coord_equal() +
+  facet_wrap(.~month, ncol = 11) +
+  scale_fill_viridis_c(option = "D")+
+  theme_void() +
+  geom_sf(data = indonesia, fill = "gray95") +
+  labs(title="SST", x="year", y="month")
+ggsave(paste0(outputdir,'nutty_plot.png'),width = 12,height = 16)
+
